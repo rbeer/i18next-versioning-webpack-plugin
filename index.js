0401cb7c1d86c75adb2b7250da8fc161adbf5a6d
Raphael Beer <raphael.beer@protonmail.com>
2019-09-12 15:21:30 +0200

    Add: switch to watchRun hook when using devServer or watching

--- index.js
+++ index.js
@@ -24,71 +24,84 @@ class I18nextVersioningPlugin {
   }
 
   static compareAndUpdate(loaded, generated) {
+    const log = I18nextVersioningPlugin.logger.info;
+
     Object.keys(generated).forEach((langKey) => {
       // ignore, if language didn't exist, yet
       if (!loaded[langKey]) {
-        I18nextVersioningPlugin.logger
-          .info(`Initializing new language ${langKey} with version ${generated[langKey].version}`);
+        log(`Initializing new language ${langKey} with version ${generated[langKey].version}`);
         return;
       }
       // increment version by one, if language does exist
       // and hashes don't match
       if (loaded[langKey].hash !== generated[langKey].hash) {
         generated[langKey].version += 1;
-        I18nextVersioningPlugin.logger
-          .info(`Incremented '${langKey}' to ${generated[langKey].version}`);
+        log(`Incremented '${langKey}' to ${generated[langKey].version}`);
       } else {
-        I18nextVersioningPlugin.logger
-          .info(`Leaving '${langKey}'   at ${generated[langKey].version}`);
+        log(`Leaving '${langKey}'   at ${generated[langKey].version}`);
       }
     });
   }
 
   static getDefinePluginOptions(compiler) {
     const defineOptions = compiler.options.plugins.filter(plugin => plugin instanceof DefinePlugin);
+    console.log(defineOptions)
     if (defineOptions.length === 0) {
       throw new ReferenceError(`${pluginName} needs DefinePlugin to work!`);
     }
     return defineOptions[0];
   }
 
+  async run(compiler, production) {
+    const developmentVersion = !production && Date.now();
+    const defineOptions = I18nextVersioningPlugin.getDefinePluginOptions(compiler);
+    const hashes = await hashElement(this.langsRoot, this.hashingOptions);
+    const generated = hashes.children.reduce((langs, child) => ({
+      ...langs,
+      [child.name]: {
+        hash: child.hash,
+        version: production
+          ? (this.hashes[child.name] && this.hashes[child.name].version) || 1
+          : developmentVersion
+      }
+    }), {});
+    // increment version and write hashes in production, only
+    if (production) {
+      I18nextVersioningPlugin.compareAndUpdate(this.hashes, generated);
+      compiler.outputFileSystem.writeFile(
+        this.hashFilePath, JSON.stringify(generated),
+        () => {}
+      );
+    } else {
+      I18nextVersioningPlugin.logger
+        .info(`Using current timestamp (${developmentVersion}) as version string for non-production build`);
+    }
+    const i18nVersions = Object.keys(generated).reduce((versions, langKey) => ({
+      ...versions,
+      [langKey]: generated[langKey].version
+    }), {});
+    // pass versions to DefinePlugin
+    defineOptions.definitions.i18nVersions = JSON.stringify(i18nVersions);
+  };
+
   apply(compiler) {
     I18nextVersioningPlugin.logger = compiler.getInfrastructureLogger(pluginName);
     const production = compiler.options.mode === 'production';
+    const watching = !!compiler.options.watch;
+    const devServer = !!process.env.WEBPACK_DEV_SERVER;
 
-    // no need to get `compiler` from the callback;
-    // it's the same object as passed to .apply
-    compiler.hooks.beforeRun.tapPromise(pluginName, async () => {
-      const developmentVersion = !production && Date.now();
-      const defineOptions = I18nextVersioningPlugin.getDefinePluginOptions(compiler);
-      const hashes = await hashElement(this.langsRoot, this.hashingOptions);
-      const generated = hashes.children.reduce((langs, child) => ({
-        ...langs,
-        [child.name]: {
-          hash: child.hash,
-          version: production
-            ? (this.hashes[child.name] && this.hashes[child.name].version) || 1
-            : developmentVersion
-        }
-      }), {});
-      // increment version and write hashes in production, only
-      if (production) {
-        I18nextVersioningPlugin.compareAndUpdate(this.hashes, generated);
-        compiler.outputFileSystem.writeFile(
-          this.hashFilePath, JSON.stringify(generated),
-          () => {}
-        );
-      } else {
-        I18nextVersioningPlugin.logger
-          .info(`Using current timestamp (${developmentVersion}) as version string for non-production build`);
-      }
-      const i18nVersions = Object.keys(generated).reduce((versions, langKey) => ({
-        ...versions,
-        [langKey]: generated[langKey].version
-      }), {});
-      // pass versions to DefinePlugin
-      defineOptions.definitions.i18nVersions = JSON.stringify(i18nVersions);
-    });
+    // `beforeRun` event doesn't fire in watch mode
+    if (watching || devServer) {
+      compiler.hooks.watchRun.tapPromise(
+        pluginName,
+        this.run.bind(this, compiler, production)
+      );
+    } else {
+      compiler.hooks.beforeRun.tapPromise(
+        pluginName,
+        this.run.bind(this, compiler, production)
+      );
+    }
   }
 }
 
